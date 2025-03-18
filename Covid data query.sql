fe377dd931afa4f4fb0f9b6b30947afbad02cde6
Mostakim Faiyaz <76640642+mostakimFaiyaz@users.noreply.github.com>
2025-03-19 01:20:10 +0600

    Add files via upload

--- Covid data query.sql
+++ Covid data query.sql
@@ -43,9 +43,61 @@ where continent is not null
 order by 1,2
 
 
-select *
+
+select cda.continent, cda.location, cda.date, cda.population, cva.new_vaccinations
+, SUM(CONVERT(int,cva.new_vaccinations))
+	OVER (
+		Partition by cda.Location Order by cda.location, cda.Date
+		) as RollingPeopleVaccinated
 from portfolioProject..CovidDeaths cda
 join portfolioProject..CovidVaccinations cva
 	on cda.location = cva.location
 	and cda.date = cva.date
 where cda.continent is not null
+order by 2,3
+
+
+
+with popVSvac (continent, location, date, population, new_vaccinations, RollingPeopleVaccinated)
+as (
+select cda.continent, cda.location, cda.date, cda.population, cva.new_vaccinations
+, SUM(CONVERT(int,cva.new_vaccinations))
+	OVER (
+		Partition by cda.Location Order by cda.location, cda.Date
+		) as RollingPeopleVaccinated
+from portfolioProject..CovidDeaths cda
+join portfolioProject..CovidVaccinations cva
+	on cda.location = cva.location
+	and cda.date = cva.date
+where cda.continent is not null
+)
+select *, (RollingPeopleVaccinated/population)*100 as vacRate
+from popVSvac
+order by vacRate
+
+
+
+drop table if exists percentPopulationVaccinated
+create table percentPopulationVaccinated (
+continent nvarchar(255),
+location nvarchar(255),
+date datetime,
+population numeric,
+new_vaccination numeric,
+RollingPeopleVaccinated numeric
+)
+
+insert into percentPopulationVaccinated
+select cda.continent, cda.location, cda.date, cda.population, cva.new_vaccinations
+, SUM(CONVERT(int,cva.new_vaccinations))
+	OVER (
+		Partition by cda.Location Order by cda.location, cda.Date
+		) as RollingPeopleVaccinated
+from portfolioProject..CovidDeaths cda
+join portfolioProject..CovidVaccinations cva
+	on cda.location = cva.location
+	and cda.date = cva.date
+where cda.continent is not null
+
+select *, (RollingPeopleVaccinated/population)*100 as vacRate
+from percentPopulationVaccinated
